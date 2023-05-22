494cbd5ccb59166615e8e74ff69b0325a337036b
Yumo Luo <yumo.luo@helsinki.fi>
2023-05-22 12:50:24 +0300

    test: unit test for APIs of fetching a station list

--- test/station/upload.test.js
+++ test/station/upload.test.js
@@ -1,7 +1,8 @@
 const { deleteAllStations, countStation, uploadStations } = require('../testHelper')
 const path = require('path')
+const server = require('../server')
 
-describe('Upload a station CSV file', () => {
+describe('Page APIs', () => {
   beforeEach(async () => {
     await deleteAllStations()
   })
@@ -15,4 +16,12 @@ describe('Upload a station CSV file', () => {
     expect(stationCount).toBe(3)
   })
 
+  test('Can list stations given a page number', async() => {
+    const filePath = path.join(__dirname, '..', 'test_data', 'station', 'station_sample_normal.csv')
+    await uploadStations(filePath)
+    const res1 = await server.get('/api/station/all/1')
+    expect(res1.body.errno).toBe(0)
+    expect(res1.body.data.count).toBe(4)
+    expect(res1.body.data.stationList.length).toBe(2)
+  })
 })
